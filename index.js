d5a4a8e69437d553ef04675f051988c681bdd208
Jee Hyun Wang <jw3046@columbia.edu>
2014-12-04 12:26:15 -0500

    "modified a href and dropdowns for html files"

--- index.js
+++ index.js
@@ -4,30 +4,26 @@ var mostPopularKey = "1afeb582103aac31d8d6a2724f32f5aa:7:70179591";
 var movieReviewsKey =" bdfdb8e3cb88fed3029cb5a65646f6f5:18:70179591";
 var topStoriesKey = "d84ffb842774795b60d4193a6c9e86c2:12:70179591";
 
+
+
 /*
-* retrieves evets data
+* example request URI's 
+*/ 
+var basicrequestURI = "http://api.nytimes.com/svc/events/v2/listings.json?api-key=5d65a900f394c8d1f5a2276a9a287120%3A12%3A70179591";
+var searchbycategoryURI = "http://api.nytimes.com/svc/events/v2/listings.json?filters=category%3AforChildren&api-key=5d65a900f394c8d1f5a2276a9a287120%3A12%3A70179591";
+
+/*
+* retrieves events data
 */
 function getEventListings(eventListingsKey) { 
-    var requestURI = "http://api.nytimes.com/svc/events/v2/listings.json?api-key=5d65a900f394c8d1f5a2276a9a287120%3A12%3A70179591";
-    var offset = (pagenumber -1) * 25
-    $("#pagetitle").empty();
-    $("#display").empty();
-    var titleofthepage="District12";
-    $("#pagetitle").append(titleofthepage);
-    var oldnewtop="";
-    oldnewtop += "                    <nav>";
-    oldnewtop += "                        <ul class=\"pager\">";
-    oldnewtop += "                            <li class=\"previous\"><a href=\"#\" id =\"newer\" onClick=\"newerF()\"><span aria-hidden=\"true\">&larr;<\/span> newer<\/a><\/li>";
-    oldnewtop += "                            <li class=\"next\"><a href=\"#\" id=\"older\" onClick=\"olderF()\">older <span aria-hidden=\"true\">&rarr;<\/span><\/a><\/li>";
-    oldnewtop += "                        <\/ul>";
-    oldnewtop += "                    <\/nav>";
-    $("#display").append(oldnewtop);
+    var requestURI = "http://api.nytimes.com/svc/events/v2/listings.jsonp?api-key=5d65a900f394c8d1f5a2276a9a287120%3A12%3A70179591";
+
     $.ajax({
         type: "GET",
-        url: pageurl + offset + com_key,
+        url: requestURI,
         dataType: 'jsonp',
         success: function (data) {
-            formatEventListings(data);
+            
             console.log(data);
         },
         error: function () {
@@ -35,4 +31,9 @@ function getEventListings(eventListingsKey) {
         }
     });
 }
+
+
+
+
+getEventListings()
 
