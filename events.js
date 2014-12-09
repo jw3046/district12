5f5153f8ef65848cc93503d606515d3b2b0242f6
dln2111 <dln2111@columbia.edu>
2014-12-09 17:54:18 -0500

    minor edits

--- events.js
+++ events.js
@@ -70,12 +70,18 @@ $(document).ready(function(){
 
 	getEventsListings();
 
+
+
+
+
 	$(document).on('click', '.btn-add-to-calendar', function(event) {
 		var e = events[$(this).attr('id')];
 		storeEvent(e.event_id, e.event_name, formatDate(e), e.web_description, e.category);
+				
 		$(this).text('Added to Calendar');
 		$(this).attr('class', 'btn btn-success disabled');
 	});
+
 	$(document).on('click', '.btn-see-related', function(event) {
 		var e = events[$(this).attr('id')];
 		// storeEvent(e.event_id, e.event_name, formatDate(e), e.web_description, e.category);
