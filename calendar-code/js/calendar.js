a682e97dd9dc94c376015931a88a6656fc8b449b
Fei-Tzin Lee <feitzin@gmail.com>
2014-12-09 19:12:40 -0500

    cleaned up alerts

--- calendar-code/js/calendar.js
+++ calendar-code/js/calendar.js
@@ -6,21 +6,15 @@
  * Updates currently stored events.
  */
 function updateEvents() {
-	alert('beginning of update');
     var e = {};
 	
 	store.forEach(function(id, event) {
-		alert('in forEach ' + event.name);
 		e[event.date] = {
 			'number': event.name,
 			'url': event.url
 		};
-		
-		//alert('e: ' + JSON.stringify(e));
 
 	});
-	
-	//alert('e: ' + JSON.stringify(e));
 	
 	return e;
 } 
